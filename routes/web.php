193fef44c175d76c558ca2b16edd54a8279bfba8
FIRGI WARDANAS <firgiwardanas257@gmail.com>
2026-04-26 00:34:54 +0700

    remove-routes(Firgi)

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ Route::get('/laravel', function () {
 });
 
 
-// Route::get('/admin/dashboard', function () {
-//     return view('admin.dashboard');
-// });
-
 Route::resource('/admin/dashboard',DashboardController::class);
 
 Route::resource('/admin/program-studi',ProgramStudiController::class);
@@ -29,28 +25,5 @@ Route::resource('/admin/login',LoginController::class);
 
 Route::resource('/',JurusanController::class);
 
-
-
-
-
-// Route::get('/admin/program-studi', function () {
-//     return view('admin.program-studi');
-// });
-
-// Route::get('/admin/akun', function () {
-//     return view('admin.akun');
-// });
-
-// Route::get('/admin/profil', function () {
-//     return view('admin.profil');
-// });
-
-// Route::get('/admin/login', function () {
-//     return view('admin/login');
-// });
-
-// Route::get('/', function () {
-//     return view('jurusan.jurusan-informatika');
-// });
 
 
