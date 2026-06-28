226d3c642773c14d7116cd4b95330e0113daa502
FIRGI WARDANAS <firgiwardanas257@gmail.com>
2026-06-28 13:52:16 +0700

    set pag kajur

--- app/Http/Controllers/ProgramStudiController.php
+++ app/Http/Controllers/ProgramStudiController.php
@@ -21,7 +21,7 @@ class ProgramStudiController extends Controller
         $query->where('kode_prodi', 'LIKE', "%{$search}%")
                 ->orWhere('nama_prodi', 'LIKE', "%{$search}%");
         })
-        ->paginate(2)
+        ->paginate(6)
         ->withQueryString();
 
         return view('admin.ketua_jurusan.program-studi', compact('prodi','search'));
